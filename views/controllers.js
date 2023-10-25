847d6d73cf74f28dd979756eae4d4f5b653dc8ee
MartellOnell <mt102005@mail.ru>
2023-10-25 09:54:05 +0500

    rework full repo

--- views/controllers.js
+++
@@ -1,5 +0,0 @@
-import express from "express"
-
-export const hello = async (req, res) => {
-    res.json("hello")
-}
--- views/router.js
+++
@@ -1,6 +0,0 @@
-import express from "express"
-import { hello } from "./controllers.js"
-export const router = express.Router()
-router.get("/", async (req, res) => {
-    await hello(req, res)
-})
