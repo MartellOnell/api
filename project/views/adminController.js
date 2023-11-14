5fc2ea2bb341d73ff66fba894b0ff43a24467e33
MartellOnell <mt102005@mail.ru>
2023-11-14 16:27:30 +0500

    review controller
    add return object in getAdminsByUsernameOrEmail

--- project/views/adminController.js
+++ project/views/adminController.js
@@ -128,7 +128,7 @@ export const getAdminsByUsernameOrEmail = async (req, res) => {
 
             const usersEmail = await User.findAll({where: whereForEmail})
             const usersUsername = await User.findAll({where: whereForUsername})
-            const users = usersEmail.concat(usersUsername)
+            const users = {username: usersUsername, email: usersEmail}
 
             return res.json({
                 msg: "successfully get data", 
