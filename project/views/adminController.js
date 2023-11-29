36136e8a5c1cbbc2c5feea2db5942e1ceab6c230
MartellOnell <mt102005@mail.ru>
2023-11-29 20:57:38 +0500

    fix

--- project/views/adminController.js
+++ project/views/adminController.js
@@ -220,7 +220,8 @@ export const editUser = async (req, res) => {
                 id: newData.id,
                 permissions: "default"
             }
-        })[0]
+        })
+        user = user[0]
         user.username = newData.username
         user.password = newData.password
         user.email = newData.email
